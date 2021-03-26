2984cc88110318b972f5d3140f530fe112ebe8ca
Ridwan ONIKOYI <onikoyiridwan@gmail.com>
2021-03-27 00:55:44 +0100

    Add model for User and Task

---
+++ src/database/migrations/20210326234543-create-task.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['pending', 'in-progress', 'completed'],
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  // eslint-disable-next-line no-unused-vars
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  },
+};
