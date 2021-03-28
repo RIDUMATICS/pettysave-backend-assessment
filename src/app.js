caf41180d6ca460d9a61e92a4183cf1484886150
Ridwan ONIKOYI <onikoyiridwan@gmail.com>
2021-03-28 15:26:27 +0100

    setup swagger documentation for all routes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
 const authRouter = require('./routes/auth');
 const taskRouter = require('./routes/v1/task');
 const passportConfig = require('./config/passport');
+const swaggerDocumentV1 = require('../swagger.v1.json');
 
 const app = express();
 
@@ -18,8 +20,10 @@ passportConfig(passport);
 
 app.use('/auth', authRouter);
 
-// Plug in the JWT strategy as a middleware so only verified users can access this route.
-app.use('/api/v1', passport.authenticate('jwt', { session: false }), taskRouter);
+app.use('/api/v1', taskRouter);
+
+// route for swagger documentation
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentV1));
 
 app.all('*', (request, response) => {
   response.status(404).json({
